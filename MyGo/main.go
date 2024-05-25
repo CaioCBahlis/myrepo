3306167812bf174e05e8ab69adeeebce33bd9818
Caio Bahlis <cbahlis@usf.edu>
2024-05-24 21:29:35 -0300

    commitado

--- MyGo/main.go
+++ MyGo/main.go
@@ -2,41 +2,56 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"time"
+  "encoding/json"
+  
 
 	"github.com/gocolly/colly/v2"
 	"github.com/imroc/req/v3"
 )
 
 
-type MyItem struct{
-  Source string
-  Price float32
-  Rating string
-  Category string
-  Url string
+type Item struct {
+	Title      string `json:"title"`
+	Price      string `json:"price"`
+	Reviews    string `json:"reviews"`
+	ProductUrl string `json:"product_url"`
+	IMGUrl     string `json:"img_url"`
+  seller     string `json:"seller"`
 }
 
+
 /////////////////////////////////////////////////////////////////
-// TO DO: Implement Random Impersonation
+// TO DO: Implement Random Impersonation #Not Needed
 // TO DO: Implement 4 Sized Scrapping #FIXED
-// TO DO: Implement DataBase
+// TO DO: Implement DataBase # Low Priority
 // TO DO: Implement Parsing based on characteristics #FIXED
 // TO DO: Implement Multiple Pages  #FIXED
-// TO DO: Implement GoRoutines
-// TO DO: Implement Search Mode
+// TO DO: Implement GoRoutines #To Be Implemented on Main File
+// TO DO: Implement Search Mode #Fixed
+// TO DO: Return type
 /////////////////////////////////////////////////////////////////
 
 
 
 
-func main(){
+func (){
+
+  fmt.Println("Search on Amazon da Shoppe:")
+  var searchreq string
+  fmt.Scanln(&searchreq)
+  URL := constructAmazonSearchURL(searchreq)
+
+  fmt.Println("Max_Depth:")
+  var MAX_DEPTH int
+  fmt.Scan(&MAX_DEPTH)
+  
 
   fakeChrome := req.DefaultClient().ImpersonateFirefox()
-  MAX_DEPTH := 1
+  
   c := colly.NewCollector(
   colly.UserAgent(fakeChrome.Headers.Get("user-agent")),
   )
@@ -45,13 +60,11 @@ func main(){
   Transport: fakeChrome.Transport,
   })
 
-  URL := "https://www.amazon.com/s?k=garrafa&crid=9QANVE34TVD7&sprefix=garra%2Caps%2C268&ref=nb_sb_noss_2"
-
   for i := 0; i != MAX_DEPTH; i++{
     Scrape(c)
+
     c.OnHTML("div.a-section.a-text-center.s-pagination-container", func(divnext *colly.HTMLElement){
       URL = "https://www.amazon.com/" + divnext.ChildAttr("a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator","href")
-  
     })
 
     time.Sleep(time.Duration(rand.Int31n(500)) * time.Millisecond)
@@ -59,10 +72,12 @@ func main(){
 
   }
 
-  
+  c.OnResponse(func(r *colly.Response){
+    fmt.Println(r.StatusCode)
+  })
  
   c.OnRequest(func(r *colly.Request) {
-      log.Println("Visiting", r.URL)
+      fmt.Println("Visiting", r.URL)
     })
     
   c.Visit(URL)
@@ -71,41 +86,62 @@ func main(){
 
 
 func Scrape(c *colly.Collector){
-  c.OnHTML("div.puis-card-container.s-card-container.s-overflow-hidden.aok-relative.puis-include-content-margin.puis.puis-vul871yo2a6ad24liafm30m715.s-latency-cf-section.puis-card-border", func(e *colly.HTMLElement) {
-
-
+  
+  c.OnHTML("div[data-component-type=s-search-result]", func(e *colly.HTMLElement) {
+    var MyItem Item
+    
     e.ForEach("div.a-section.a-spacing-small.a-spacing-top-small", func(i int, SingleGrid *colly.HTMLElement) {
 
       //fmt.Println(SingleGrid.Text)
       TitleParent := SingleGrid.DOM.Find("div[data-cy=title-recipe]")
-      fmt.Println("Title:",  TitleParent.Find("span.a-size-medium.a-color-base.a-text-normal").Text())
       PriceParent := SingleGrid.DOM.Find("span.a-price[data-a-size='xl']")
-      fmt.Println("Price:", PriceParent.Find("span.a-offscreen").Text())
-      fmt.Println("Reviews:", SingleGrid.ChildText("span.a-icon-alt"))
+
+      MyItem.Title = TitleParent.Find("span.a-size-medium.a-color-base.a-text-normal").Text()
+      MyItem.Price = PriceParent.Find("span.a-offscreen").Text()
+      MyItem.Reviews = SingleGrid.ChildText("span.a-icon-alt")
+    
 
     })
     
-    
-      
-    e.ForEach("a.a-link-normal.s-no-outline", func(i int, singleURL *colly.HTMLElement) {
-        fmt.Println("ProductUrl: https://www.amazon.com", singleURL.Attr("href"))
-
-        fmt.Println("URL:", singleURL.ChildAttr("img.s-image","src"))
-    })
-      
-
-    fmt.Println("--------------------------") 
-
+  
     e.ForEach("div.a-section.a-spacing-small.puis-padding-left-small.puis-padding-right-small", func(i int, quadgrid *colly.HTMLElement) {
       TitleParent := quadgrid.DOM.Find("div[data-cy=title-recipe]")
-      fmt.Println("Title:",  TitleParent.Find("span.a-size-base-plus.a-color-base.a-text-normal").Text())
       PriceParent := quadgrid.DOM.Find("span.a-price[data-a-size='xl']")
-      fmt.Println("Price:", PriceParent.Find("span.a-offscreen").Text())
-      fmt.Println("Reviews:", quadgrid.ChildText("span.a-icon-alt"))
+
+      if MyItem.Title == ""{
+        MyItem.Title =  TitleParent.Find("span.a-size-base-plus.a-color-base.a-text-normal").Text()
+        MyItem.Price =  PriceParent.Find("span.a-offscreen").Text()
+        MyItem.Reviews =  quadgrid.ChildText("span.a-icon-alt")
+       }
+      
     })
 
+    e.ForEach("a.a-link-normal.s-no-outline", func(i int, singleURL *colly.HTMLElement) {
+      MyItem.ProductUrl = "https://www.amazon.com" + singleURL.Attr("href")
+      MyItem.IMGUrl = singleURL.ChildAttr("img.s-image","src")
+      MyItem.seller = "Amazon"
+      
   })
+
+  JsonVal, err := json.Marshal(MyItem)
+  if err != nil{
+    fmt.Println("error converting object to json")
+  }
+
+  fmt.Println("--------------------------------------")
+  fmt.Println(string(JsonVal))
+  fmt.Println("--------------------------------------")
+
+  })
+
 }
 
+func constructAmazonSearchURL(product string) string {
+	baseURL := "https://www.amazon.com/s"
+	params := url.Values{}
+	params.Add("k", product)
+	params.Add("ref", "nb_sb_noss_2")
+	return fmt.Sprintf("%s?%s", baseURL, params.Encode())
+}
 
 
